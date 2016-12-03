efd4282af2ec25251f03889e58631eb2979e3c69
me <mr.lolans.mail@gmail.com>
2016-12-03 17:59:13 +0300

    Добавьте файлы проекта.

---
+++ CSChess/Login.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ChessLibrary;
+
+namespace CSChess
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        Internet Internet = new ChessLibrary.Internet();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        void Connect()
+        {
+            try
+            {
+                Internet.Connect(CSChess.Properties.Resources.IP);
+            }
+            catch(Exception err)
+            {
+                MessageBox.Show("Ошибка подключения.\nПодробнее: " + err.Message, "Ошибка");
+            }
+        }
+
+        private void DisableBtns()
+        {
+            LoginBtn.IsEnabled = false;
+            RegisterBtn.IsEnabled = false;
+            OfflineBtn.IsEnabled = false;
+        }
+
+        private void EnableBtns()
+        {
+            LoginBtn.IsEnabled = true;
+            RegisterBtn.IsEnabled = true;
+            OfflineBtn.IsEnabled = true;
+        }
+
+        private void OpenGameWindow()
+        {
+            Close();
+            //Новое окно
+        }
+
+        private async void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DisableBtns();
+            bool res = await LoginMain(LoginText.Text, PasswordText.Text);
+            EnableBtns();
+            if (res) OpenGameWindow();
+        }
+
+        Task<bool> LoginMain(string login, string pass)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    if (!Internet.IsConnected) Internet.Connect(CSChess.Properties.Resources.IP);
+                    Internet.Login(login, pass);
+                    return true;
+                }
+                catch (ChessLibrary.Internet.Exceptions.UserLoginError err)
+                {
+                    MessageBox.Show("Ошибка входа.\nПодробнее: " + err.Message, "Ошибка");
+                    return false;
+                }
+                catch (System.Net.Sockets.SocketException err)
+                {
+                    MessageBox.Show("Ошибка подключения.\nПодробнее: " + err.Message, "Ошибка");
+                    return false;
+                }
+            });
+        }
+
+        private async void RegisterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DisableBtns();
+            bool res = await RegisterMain(LoginText.Text, PasswordText.Text);
+            EnableBtns();
+            if (res) OpenGameWindow();
+        }
+        Task<bool> RegisterMain(string login, string pass)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    if (!Internet.IsConnected) Internet.Connect(CSChess.Properties.Resources.IP);
+                    Internet.Register(login, pass);
+                    return true;
+                }
+                catch (ChessLibrary.Internet.Exceptions.UserAlreadyRegistred err)
+                {
+                    MessageBox.Show("Ошибка регистрации.\nПодробнее: " + err.Message, "Ошибка");
+                    return false;
+                }
+                catch (System.Net.Sockets.SocketException err)
+                {
+                    MessageBox.Show("Ошибка подключения.\nПодробнее: " + err.Message, "Ошибка");
+                    return false;
+                }
+            });
+        }
+    }
+}
---
+++ ChessLibrary/Internet.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLibrary
+{
+    public class User
+    {
+        public class ID
+        {
+            private byte group;
+            private byte id;
+            //public int Group { get group; }
+            //public int Id { get id; }
+
+            public ID(byte[] bytes)
+            {
+                group = bytes[0];
+                id = bytes[1];
+            }
+
+            override
+            public string ToString()
+            {
+                return group.ToString() + id.ToString();
+            }
+            public byte[] ToBytes()
+            {
+                return new byte[] { group, id };
+            }
+        }
+        //Статистика
+        //Еще что-то
+        public ID id { get; set; }
+    }
+    public class Internet
+    {
+        public static class Exceptions
+        {
+            public class UserLoginError : Exception
+            {
+                public UserLoginError(string message) : base(message) { }
+            }
+            public class UserAlreadyJoined : UserLoginError
+            {
+                public UserAlreadyJoined(string message) : base(message) { }
+            }
+            public class UserAlreadyRegistred : UserLoginError
+            {
+                public UserAlreadyRegistred(string message) : base(message) { }
+            }
+        }
+        Socket socket;
+        public Internet() { }
+        private bool isLogged;
+        public bool IsLogged { get { return isLogged; } }
+        private bool isConnected;
+        public bool IsConnected { get { return isConnected; } }
+        public void Connect(string IP)
+        {
+            IPAddress ipAddr = IPAddress.Parse(IP);
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11223);
+
+            socket = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(ipEndPoint);
+            isConnected = true;
+        }
+
+        private byte[] DataToArray(byte type, string login, string password)
+        {
+            List<byte> data = new List<byte>();
+            data.Add(type);
+            data.AddRange(Encoding.UTF8.GetBytes(login + "/" + password));
+            return data.ToArray();
+        }
+
+        public User Login(string login, string password)
+        {
+            socket.Send(DataToArray(1, login, password));
+            byte[] receive = new byte[64];
+            int lenght = socket.Receive(receive);
+            if (receive[0] == 1)
+                throw new Exceptions.UserAlreadyJoined("User has already joined");
+            else if (receive[0] == 2)
+                throw new Exceptions.UserLoginError("Password or/and login incorrect");
+            else
+            {
+                //string rec = Encoding.UTF8.GetString(receive, 1, lenght);
+                //string[] recData = rec.Split('/');  //TODO
+                isLogged = true;
+                return new User();
+            }
+        }
+
+        public void Register(string login, string password)
+        {
+            socket.Send(DataToArray(2, login, password));
+            byte[] receive = new byte[1];
+            socket.Receive(receive);
+            if (receive[0] == 2)
+                throw new Exceptions.UserAlreadyRegistred("User has already registered");
+            isLogged = true;
+            return;
+        }
+
+        public User.ID GetID(string login, string password)
+        {
+            socket.Send(new byte[] {0});
+            byte[] Receive = new byte[2];
+            socket.Receive(Receive);
+            return new User.ID(Receive);
+        }
+    }
+}
